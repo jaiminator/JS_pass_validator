6d3078d0f0d168e1a7d8f526ce649e693ddbf167
David <114335539+olisito@users.noreply.github.com>
2025-02-07 13:45:13 +0100

    Revisiondavid (#1)
    * añadimos placerholder y mas estilos en css
    
    * ya funciona el boton solo cuando es segura la clave y aparece la alerta con el user
    
    * trabajo finalizado

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const inputText = document.getElementById("inputPass")
-inputText.addEventListener("keyup", validar)
-const validatorText = document.getElementById("textSecurity")
-
+const inputText = document.getElementById("inputPass");
+inputText.addEventListener("keyup", validar);
+const validatorText = document.getElementById("textSecurity");
+const buttonlogin = document.getElementById("buttonlogin");
+buttonlogin.addEventListener("click", alerta);
+const user = document.getElementById("inputuser");
+user.addEventListener("keyup", validar);
 function validar() {
-    if (inputText.value.length < 8) {
-        validatorText.style.color = "red"
-        validatorText.textContent = "La clave no es segura"
-    } else if (inputText.value.length >= 8 && inputText.value.length <= 10){
-        validatorText.style.color = "blue"
-        validatorText.textContent = "La clave es segura"
+    if (inputText.value.length == 0 && user.value.length == 0) {
+        validatorText.textContent = "";
+    } else if (inputText.value.length == 0) {
+        validatorText.textContent = "Falta clave.";
+        validatorText.style.color = "red";
+    } else if (user.value.length == 0) {
+        validatorText.textContent = "Falta Usuario.";
+        validatorText.style.color = "red";
+    } else if (inputText.value.length < 8) {
+        validatorText.style.color = "red";
+        validatorText.textContent = "La clave no es segura.";
+        buttonlogin.disabled = true;
+    } else if (inputText.value.length >= 8 && inputText.value.length <= 10) {
+        validatorText.style.color = "blue";
+        validatorText.textContent = "La clave es segura.";
+        buttonlogin.disabled = true;
     } else {
-        validatorText.style.color = "green"
-        validatorText.textContent = "La clave es muy segura"
+        if (user.value == "") {
+            buttonlogin.setAttribute("disabled", "true");
+            buttonlogin.setAttribute("class", "botonred");
+        } else {
+            buttonlogin.removeAttribute("disabled");
+            buttonlogin.setAttribute("class", "botonverde");
+        }
+        validatorText.style.color = "green";
+        validatorText.textContent = "La clave es muy segura.";
     }
 }
-
+function alerta() {
+    alert("Hola, " + user.value);
+}
