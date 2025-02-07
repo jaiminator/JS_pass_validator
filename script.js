9d2f74907258ef672557eaccd578954514e2bd89
david_oliver <daviid_oliver@hotmail.com>
2025-02-07 10:08:09 +0100

    añadimos placerholder y mas estilos en css

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const inputText = document.getElementById("inputPass")
-inputText.addEventListener("keyup", validar)
-const validatorText = document.getElementById("textSecurity")
+const inputText = document.getElementById("inputPass");
+inputText.addEventListener("keyup", validar);
+const validatorText = document.getElementById("textSecurity");
 
 function validar() {
     if (inputText.value.length < 8) {
-        validatorText.style.color = "red"
-        validatorText.textContent = "La clave no es segura"
-    } else if (inputText.value.length >= 8 && inputText.value.length <= 10){
-        validatorText.style.color = "blue"
-        validatorText.textContent = "La clave es segura"
+        validatorText.style.color = "red";
+        validatorText.textContent = "La clave no es segura.";
+    } else if (inputText.value.length >= 8 && inputText.value.length <= 10) {
+        validatorText.style.color = "blue";
+        validatorText.textContent = "La clave es segura.";
     } else {
-        validatorText.style.color = "green"
-        validatorText.textContent = "La clave es muy segura"
+        validatorText.style.color = "green";
+        validatorText.textContent = "La clave es muy segura.";
     }
 }
-
