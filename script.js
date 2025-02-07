b1ee36a14d2cfa1d0bb8970acd29bdf573fd1bba
david_oliver <daviid_oliver@hotmail.com>
2025-02-07 13:06:21 +0100

    trabajo finalizado

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ const validatorText = document.getElementById("textSecurity");
 const buttonlogin = document.getElementById("buttonlogin");
 buttonlogin.addEventListener("click", alerta);
 const user = document.getElementById("inputuser");
-
+user.addEventListener("keyup", validar);
 function validar() {
-    if (inputText.value.length < 8) {
+    if (inputText.value.length == 0 && user.value.length == 0) {
+        validatorText.textContent = "";
+    } else if (inputText.value.length == 0) {
+        validatorText.textContent = "Falta clave.";
+        validatorText.style.color = "red";
+    } else if (user.value.length == 0) {
+        validatorText.textContent = "Falta Usuario.";
+        validatorText.style.color = "red";
+    } else if (inputText.value.length < 8) {
         validatorText.style.color = "red";
         validatorText.textContent = "La clave no es segura.";
+        buttonlogin.disabled = true;
     } else if (inputText.value.length >= 8 && inputText.value.length <= 10) {
         validatorText.style.color = "blue";
         validatorText.textContent = "La clave es segura.";
+        buttonlogin.disabled = true;
     } else {
-        buttonlogin.removeAttribute("disabled");
+        if (user.value == "") {
+            buttonlogin.setAttribute("disabled", "true");
+            buttonlogin.setAttribute("class", "botonred");
+        } else {
+            buttonlogin.removeAttribute("disabled");
+            buttonlogin.setAttribute("class", "botonverde");
+        }
         validatorText.style.color = "green";
         validatorText.textContent = "La clave es muy segura.";
     }
@@ -21,4 +37,3 @@ function validar() {
 function alerta() {
     alert("Hola, " + user.value);
 }
-function botonlogin() {}
